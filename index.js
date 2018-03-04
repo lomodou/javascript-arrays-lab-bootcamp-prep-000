09531533decd3c7e8c940432e0b5a4f21cd56450
Modou Lo <modoulo99@icloud.com>
2018-03-04 07:49:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 const app = "I don't do much."
 
-var kittens = ["Milo", "Otis", "Garfield"]
+var kittens = ["Milo", "Otis", "Garfield"];
 
 function destructivelyAppendKitten(name) {
-  kittens.push(name)
-  return kittens
+  kittens.push(name);
+  return kittens;
 }
 
 function destructivelyPrependKitten(name) {
-  kittens.unshift(name)
-  return kittens
+  kittens.unshift(name);
+  return kittens;
 }
 
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+  return kittens;
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+  return kittens;
+}
